f0a55a9360ab8eb173c9c71fbdc2b5d25a291bc1
Vinh Van <tavanvinh172@gmail.com>
2023-03-10 19:07:51 +0700

    footer + content + animation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let slideIndex = 0;
 showSlides();
-
+window.onscroll = function() {
+  scrollFunction(); 
+};
 function plusSlides(n){
   showSlides(slideIndex += n);
 }
@@ -8,7 +10,7 @@ function plusSlides(n){
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("banner-image");
-
+  let mainTitle = document.getElementById("banner-main-title");
   if(n > slides.length) {slideIndex = 1}
   if(n < 1) {slideIndex = slides.length}
 
@@ -18,5 +20,25 @@ function showSlides(n) {
   slideIndex++;
   if(slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex - 1].style.display = "block";
+  // slides[slideIndex - 1].textContent = 'fehfiok';
+  if(slideIndex - 1 === 0){
+    mainTitle.textContent = "Find Alley | More";
+  }else if(slideIndex - 1 === 1){
+    mainTitle.textContent = "Riverside Abode | More";
+  }else if(slideIndex - 1 === 2){
+    mainTitle.textContent = "Duet Apartment | More";
+  }else if(slideIndex - 1 === 3){
+    mainTitle.textContent = "Revival House | Echo of the body";
+  }
   setTimeout(showSlides, 5000);
+}
+
+function scrollFunction() {
+  if(document.documentElement.scrollHeight > 300){
+    document.getElementById("header").className = "slideOpaque";
+  }
+  if(document.documentElement.scrollHeight > 1400){
+    console.log(`aaaaaa: ${document.documentElement.scrollHeight}`);
+    document.getElementsByClassName("content-description").className = "slideLeft";
+  }
 }
